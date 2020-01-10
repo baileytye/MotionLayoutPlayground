1de53b596027f162100da661a78ecd3f90f2b3e1
Bailey Tye <bailey@vogcalgaryappdeveloper.com>
2020-01-10 16:33:48 -0700

    Add animation to text between screens
    Signed-off-by: Bailey Tye <bailey@vogcalgaryappdeveloper.com>

--- app/src/main/java/com/example/motionlayoutplayground/welcome/OnboardingSceneView.kt
+++ app/src/main/java/com/example/motionlayoutplayground/welcome/OnboardingSceneView.kt
@@ -12,6 +12,34 @@ class OnboardingSceneView @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : MotionLayout(context, attrs, defStyleAttr) {
 
+    init {
+        setTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {
+
+            }
+
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
+            }
+
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {
+                title.text = when (p3) {
+                    in 0f..0.25f -> "Welcome to Buildster"
+                    in 0.25f..0.75f -> "Instantly Receive Updates"
+                    else -> "Messaging"
+                }
+                message.text = when (p3) {
+                    in 0f..0.25f -> "Visit the Projects screen to upload, share, and access building plans\n\nManage Tasks on the plans with team members\n\nCreate tasks for yourself and your team"
+                    in 0.25f..0.75f -> "Post project updates or news to your team\n\nView, respond, and comment on open tasks\n\nSend realtime notifications to the team"
+                    else -> "Keep your messages organized in each project\n\nHelps you connect with others in your projects\n\nEncourages growth"
+                }
+            }
+
+            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+            }
+
+        })
+    }
+
     val pageListener = PageListener()
 
     inner class PageListener : ViewPager2.OnPageChangeCallback() {
@@ -22,24 +50,6 @@ class OnboardingSceneView @JvmOverloads constructor(
         ) {
             val numPages = 3
             progress = (position + positionOffset) / (numPages - 1)
-
-            when (position) {
-                0 -> {
-                    title.text = "Welcome to Buildster"
-                    message.text =
-                        "Visit the Projects screen to upload, share, and access building plans\n\nManage Tasks on the plans with team members\n\nCreate tasks for yourself and your team"
-                }
-                1 -> {
-                    title.text = "Instantly Receive Updates"
-                    message.text =
-                        "Post project updates or news to your team\n\nView, respond, and comment on open tasks\n\nSend realtime notifications to the team"
-                }
-                2 -> {
-                    title.text = "Messaging"
-                    message.text =
-                        "Keep your messages organized in each project\n\nHelps you connect with others in your projects\n\nEncourages growth"
-                }
-            }
         }
     }
 }
